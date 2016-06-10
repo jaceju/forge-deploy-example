31045b408c3c3115a30ec2fda1f4b44b31a708e8
Jace Ju <jaceju@gmail.com>
2016-06-11 00:33:45 +0800

    Update page title for experiment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Forge Deploy Example</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Hello Forge!!</div>
             </div>
         </div>
     </body>
